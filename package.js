6584a7fc5312deaa09713540855690364d902147
Jack Lukic <jack@semantic-ui.com>
2015-02-18 14:25:53 -0500

    Updated component to version 1.9.0

---
+++ package.js
@@ -0,0 +1,17 @@
+var
+  where = 'client' // Adds files only to the client
+;
+
+Package.describe({
+  name    : 'semantic:ui-progress',
+  summary : 'Semantic UI - Progress (official): Single component release of progress',
+  version : '1.9.0',
+  git     : 'git://github.com/Semantic-Org/UI-Progress.git',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles([
+    
+  ], where);
+});
