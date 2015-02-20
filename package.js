2d8416c9b9ed905f5f93465f2a65fce1659a4f4b
Pusher Robot <jack@semantic-ui.com>
2015-02-20 17:16:06 -0500

    Updated component to version 1.9.3

--- package.js
+++ package.js
@@ -1,17 +1,15 @@
-var
-  where = 'client' // Adds files only to the client
-;
 
 Package.describe({
   name    : 'semantic:ui-progress',
-  summary : 'Semantic UI - Progress (official): Single component release of progress',
-  version : '1.9.2',
+  summary : 'Semantic UI - Progress: Single component release',
+  version : '1.9.3',
   git     : 'git://github.com/Semantic-Org/UI-Progress.git',
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.addFiles([
-    
-  ], where);
+    'progress.css',
+    'progress.js'
+  ], 'client');
 });
