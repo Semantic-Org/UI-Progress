2092c44177811be458c621fbcfd9962f958a82a8
Pusher Robot <jack@semantic-ui.com>
2015-03-05 11:03:58 -0500

    Updated component to version 1.11.0

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-progress',
   summary : 'Semantic UI - Progress: Single component release',
-  version : '1.10.3',
+  version : '1.11.0',
   git     : 'git://github.com/Semantic-Org/UI-Progress.git',
 });
 
