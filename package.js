83541b25dd7066e54000580775f1539e3fa4dc68
Jack Lukic <jack@semantic-ui.com>
2017-03-29 06:42:29 -0400

    Updated component to version 2.2.10

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-progress',
   summary : 'Semantic UI - Progress: Single component release',
-  version : '2.2.8',
+  version : '2.2.10',
   git     : 'git://github.com/Semantic-Org/UI-Progress.git',
 });
 
